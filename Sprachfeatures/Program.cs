9facbfe5e55e52d54ca35911d55ad1ab483ac6e2
ppedvLukasK <lukask@ppedv.de>
2022-11-28 09:53:46 +0100

    M001

---
+++ Sprachfeatures/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Sprachfeatures
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+
+		string Test()
+		{
+			int x = 0;
+			switch (x) //Strg + . für direkte Konvertierung
+			{
+				case 0: return "Null";
+				case 1: return "Eins";
+				default: return "Andere Zahl";
+			}
+		}
+
+		string Test2(string x1,string x2)
+		{
+			return (x1, x2) switch
+			{
+				("X", "Y") => "Test",
+				_ => "" //Ein _ hier ausreichend
+			};
+		}
+	}
+}
