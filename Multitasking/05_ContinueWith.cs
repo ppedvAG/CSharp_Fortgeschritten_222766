28db05af676f776657df4081b9adf32ff52ce990
ppedvLukasK <lukask@ppedv.de>
2022-11-29 16:11:30 +0100

    M007

---
+++ Multitasking/05_ContinueWith.cs
@@ -0,0 +1,19 @@
+﻿namespace Multitasking;
+
+internal class _05_ContinueWith
+{
+	static void Main(string[] args)
+	{
+		Task<double> t1 = Task.Run(() =>
+		{
+			Thread.Sleep(1000);
+			return Math.Pow(4, 23);
+		});
+		t1.ContinueWith(task => Console.WriteLine(task.Result)); //Tasks verketten, Code wird ausgeführt wenn originaler Task fertig, Variable des vorherigen Tasks inkludiert
+		t1.ContinueWith(task => Console.WriteLine(task.Result * 2), TaskContinuationOptions.OnlyOnRanToCompletion); //Dieser Pfad wird betreten wenn keine Exception aufgetreten ist, es können auch mehrere Folgetasks ausgeführt werden
+		t1.ContinueWith(task => Console.WriteLine("Exception"), TaskContinuationOptions.OnlyOnFaulted); //Wird ausgeführt bei Exception
+		t1.ContinueWith(task => Console.WriteLine(), TaskContinuationOptions.NotOnFaulted); //Wenn keine Unhandled Exception
+
+		Console.ReadKey();
+	}
+}
---
+++ Multitasking/ParallelFor.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+namespace Multitasking;
+
+internal class ParallelForDemo
+{
+	static void Main(string[] args)
+	{
+		int[] durchgänge = { 1000, 10000, 50000, 100000, 250000, 500000, 1000000, 5000000, 10000000, 100000000 };
+
+		foreach (int i in durchgänge)
+		{
+			Stopwatch sw = Stopwatch.StartNew();
+			RegularFor(i);
+			sw.Stop();
+			Console.WriteLine($"For Durchgänge {i}: {sw.ElapsedMilliseconds}");
+
+			Stopwatch sw2 = Stopwatch.StartNew();
+			ParallelFor(i);
+			sw2.Stop();
+			Console.WriteLine($"Parallel For Durchgänge {i}: {sw2.ElapsedMilliseconds}");
+		}
+
+		/*
+			For Durchgänge 1000: 0
+			Parallel For Durchgänge 1000: 24
+			For Durchgänge 10000: 1
+			Parallel For Durchgänge 10000: 22
+			For Durchgänge 50000: 6
+			Parallel For Durchgänge 50000: 7
+			For Durchgänge 100000: 13
+			Parallel For Durchgänge 100000: 4
+			For Durchgänge 250000: 40
+			Parallel For Durchgänge 250000: 16
+			For Durchgänge 500000: 86
+			Parallel For Durchgänge 500000: 26
+			For Durchgänge 1000000: 166
+			Parallel For Durchgänge 1000000: 39
+			For Durchgänge 5000000: 1840
+			Parallel For Durchgänge 5000000: 543
+			For Durchgänge 10000000: 2374
+			Parallel For Durchgänge 10000000: 636
+			For Durchgänge 100000000: 16718
+			Parallel For Durchgänge 100000000: 7549
+		 */
+	}
+
+	static void RegularFor(int iterations)
+	{
+		double[] erg = new double[iterations];
+		for (int i = 0; i < iterations; i++)
+			erg[i] = (Math.Pow(i, 0.333333333333) * Math.Sin(i + 2) / Math.Exp(i) + Math.Log(i + 1)) * Math.Sqrt(i + 100);
+	}
+
+	static void ParallelFor(int iterations)
+	{
+		double[] erg = new double[iterations];
+		//int i = 0; i < iterations; i++
+		Parallel.For(0, iterations, i => erg[i] = (Math.Pow(i, 0.333333333333) * Math.Sin(i + 2) / Math.Exp(i) + Math.Log(i + 1)) * Math.Sqrt(i + 100));
+	}
+}
