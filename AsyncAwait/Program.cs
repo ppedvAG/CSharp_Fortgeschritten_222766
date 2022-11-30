608008a587867cc97e5725cff84a887ca6dbb191
ppedvLukasK <lukask@ppedv.de>
2022-11-30 10:14:00 +0100

    M007

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Diagnostics;
+
+namespace AsyncAwait;
+
+internal class Program
+{
+	static async Task Main(string[] args)
+	{
+		//Stopwatch sw = Stopwatch.StartNew();
+		//Toast();
+		//Geschirr();
+		//Kaffee();
+		//sw.Stop();
+		//Console.WriteLine(sw.ElapsedMilliseconds); //Sequentiell, 7s
+
+		//Stopwatch sw = Stopwatch.StartNew();
+		//Task.Run(Toast);
+		//Task.Run(Geschirr);
+		//Task.Run(Kaffee);
+		//sw.Stop();
+		//Console.WriteLine(sw.ElapsedMilliseconds); //16ms, Main Thread läuft weiter
+
+		//Stopwatch sw = Stopwatch.StartNew();
+		//ToastTaskAsync(); //Methoden werden als Tasks ausgeführt weil sie als async gekennzeichnet sind
+		//GeschirrTaskAsync();
+		//KaffeeTaskAsync();
+		//sw.Stop();
+		//Console.WriteLine(sw.ElapsedMilliseconds); //12ms, Main Thread läuft weiter
+
+		//Wenn ich eine void Methode awaiten möchte muss ich diese mit Task als Rückgabewert kennzeichnen -> braucht kein return
+
+		Stopwatch stopwatch = Stopwatch.StartNew();
+		Task<Toast> toast = ToastAsync(); //Task starten
+		Task<Tasse> tasse = TasseAsync(); //Task starten
+		Tasse t = await tasse; //Warte bis die Tasse fertig ist
+		Task<Kaffee> kaffee = KaffeeAsync(t); //KaffeeAsync(await tasse);
+		Toast t2 = await toast; //Warte bis Toast fertig ist
+		Kaffee k = await kaffee; //Warte bis Kaffee fertig ist
+		stopwatch.Stop();
+		Console.WriteLine(stopwatch.ElapsedMilliseconds); //4s
+	}
+
+	static void Toast()
+	{
+		Thread.Sleep(4000);
+		Console.WriteLine("Toast fertig");
+	}
+
+	static void Geschirr()
+	{
+		Thread.Sleep(1500);
+		Console.WriteLine("Geschirr hergerrichtet");
+	}
+
+	static void Kaffee()
+	{
+		Thread.Sleep(1500);
+		Console.WriteLine("Kaffee zubereitet");
+	}
+
+	static async void ToastTaskAsync()
+	{
+		await Task.Delay(4000);
+		Console.WriteLine("Toast fertig");
+	}
+
+	static async void GeschirrTaskAsync()
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Geschirr hergerrichtet");
+	}
+
+	static async void KaffeeTaskAsync()
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Kaffee zubereitet");
+	}
+
+	static async Task<Toast> ToastAsync()
+	{
+		await Task.Delay(4000);
+		Console.WriteLine("Toast fertig");
+		return new Toast();
+	}
+
+	static async Task<Tasse> TasseAsync()
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Tasse hergerrichtet");
+		return new Tasse();
+	}
+
+	static async Task<Kaffee> KaffeeAsync(Tasse t)
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Kaffee fertig");
+		return new Kaffee();
+	}
+}
+
+public class Toast { }
+
+public class Tasse { }
+
+public class Kaffee { }
---
+++ AsyncAwaitWPF/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncAwaitWPF;
+
+public partial class MainWindow : Window
+{
+	public MainWindow() => InitializeComponent();
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Progress.Value = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			Thread.Sleep(25);
+			Progress.Value++;
+		} //Thread.Sleep verhindert UI Updates
+	}
+
+	private void Button_Click_1(object sender, RoutedEventArgs e)
+	{
+		Task.Run(() =>
+		{
+			Dispatcher.Invoke(() => Progress.Value = 0);
+			for (int i = 0; i < 100; i++)
+			{
+				Thread.Sleep(25);
+				Dispatcher.Invoke(() => Progress.Value++);
+			}
+		});
+	}
+
+	private async void Button_Click_2(object sender, RoutedEventArgs e)
+	{
+		Progress.Value = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			await Task.Delay(25);
+			Progress.Value++;
+		} //funktioniert auch ohne Dispatcher.Invoke
+	}
+
+	private async void Button_Click_3(object sender, RoutedEventArgs e)
+	{
+		using HttpClient client = new(); //mit using anlegen da IDisposable
+		HttpResponseMessage resp = await client.GetAsync(@"http://www.gutenberg.org/files/54700/54700-0.txt"); //Get Request machen mit await
+		string content = await resp.Content.ReadAsStringAsync();
+		TB.Text = content;
+	}
+
+	private async void Button_Click_4(object sender, RoutedEventArgs e)
+	{
+		List<int> ints = Enumerable.Range(0, 100).ToList(); //Schnell eine int Liste von 0-99 generieren
+		await Parallel.ForEachAsync(ints, (item, ct) => //item: Derzeitige Zahl, ct: CancellationToken, kann ignoriert werden
+		{
+			Dispatcher.Invoke(() => TB.Text += item * 10 + "\n");
+			return ValueTask.CompletedTask; //Leeren Task returnen, muss man wissen
+		});
+	}
+}
